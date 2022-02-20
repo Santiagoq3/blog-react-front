9d9678aa29cb5295d0c21e790031cd6ed6b1c084
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-02-20 03:39:43 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { DatePicker, Space,Card } from 'antd';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Admin } from './pages/admin/Admin';
+import { SignIn } from './pages/admin/SignIn';
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App</h1>
+      <Home />
+      <Admin />
+      <SignIn />
+      <Contact />
     </div>
+
   );
 }
+
 
 export default App;
